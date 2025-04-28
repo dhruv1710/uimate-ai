a899276e4a340a81a8c40318b4b09a469f2ff115
Dhruv Tiwari <dhruv.d.tiwari@gmail.com>
2025-04-28 15:56:19 +0530

    removed instruments folder

--- app/instruments/page.tsx
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@/lib/supabase/server';
-
-export default async function Instruments() {
-  const supabase = await createClient();
-  const { data: chat } = await supabase.from("chat_history").select();
-
-  return <pre>{JSON.stringify(chat, null, 2)}</pre>
-}
